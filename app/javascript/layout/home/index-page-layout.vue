163a72c7e39eeed38135eb7efa48f523f89e4bd2
soeltan <soeltan.zaky@alodokter.com>
2020-10-26 21:14:28 +0700

    add tailwindcss example of flexbox

--- app/javascript/layout/home/index-page-layout.vue
+++ app/javascript/layout/home/index-page-layout.vue
@@ -1,6 +1,10 @@
 <template>
-<div class="container mx-auto">
-  {{ data.message }}
+<div class="mx-auto">
+  <div class="flex sm:flex-col md:flex-row lg:flex-row xl:flex-row flex-wrap">
+    <div class="w-full md:w-1/4 bg-gray-400 p-4 text-center text-gray-700"> {{ data.message }}</div>
+    <div class="w-full md:w-2/4 bg-gray-500 p-4 text-center text-gray-200"> {{ data.message }}</div>
+    <div class="w-full md:w-1/4 bg-gray-400 p-4 text-center text-gray-700"> {{ data.message }}</div>
+  </div> 
 </div>
 </template>
 
