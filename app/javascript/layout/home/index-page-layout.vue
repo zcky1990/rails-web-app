623f179368847fefda11a2cda33f40d3e8005a2c
soeltan <soeltan.zaky@alodokter.com>
2020-11-10 00:14:41 +0700

    add facebook button

--- app/javascript/layout/home/index-page-layout.vue
+++ app/javascript/layout/home/index-page-layout.vue
@@ -9,25 +9,52 @@
           {{ data.message }}
         </div>
       </div>
-      <div class="w-full md:w-1/5 bg-gray-400 p-4 text-center text-gray-700">
-        {{ data.message }}
+      <div class="w-full md:w-1/5 bg-blue-400 p-4 text-center text-gray-700">
+        <facebook-login
+          class="button"
+          appId="462577024718487"
+          @login="getUserData"
+          @logout="onLogout"
+          @get-initial-status="getUserData"
+        >
+        </facebook-login>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import facebookLogin from "../../components/facebook-login/facebook-login.vue";
+
 export default {
   data: function () {
     return {
       data: {},
     };
   },
+  components: {
+    facebookLogin,
+  },
   props: {
     _datas: {},
   },
   mounted: function () {
     this.data = { ...this._datas };
+  },
+  methods: {
+    onLogout: function (response) {
+      
+    },
+    getUserData: function (response) {
+      if (response.response){
+        var authResponse = response.response.authResponse;
+        var facebookId = authResponse.userID;
+        var facebookToken = authResponse.accessToken;
+        //send ajax to login in app
+      }
+      
+      console.log("clicked")
+    },
   },
 };
 </script>
--- app/service/api/v1/sessions_service.rb
+++ app/service/api/v1/sessions_service.rb
@@ -52,7 +52,7 @@ class Api::V1::SessionsService < Api::V1::JsonService
       oauth_user_data["avatar_img_url"] = provider_data[:user_image] rescue nil
 
       new_user = User.new(oauth_user_data)
-      if new_user.save(validate: false)!
+      if new_user.save(validate: false)
         return user_result_json(SUCCESS, user, USER_SUCCESS_LOGIN)
       else
         return user_result_json(FAILED, nil, ERROR_FAILED_LOGIN)
