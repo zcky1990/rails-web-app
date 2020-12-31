80b688f0520660cb969f18a8a8d4f70e33a50af5
soeltan <soeltan.zaky@alodokter.com>
2020-12-31 18:14:09 +0700

    add pagination template on table

--- app/javascript/components/admin/table-list.vue
+++ app/javascript/components/admin/table-list.vue
@@ -68,6 +68,42 @@
             </div>
           </td>
         </tr>
+        <tr>
+          <div class="container pagination-page">
+            <nav
+              class="pagination is-right is-fullwidth"
+              role="navigation"
+              aria-label="pagination"
+            >
+              <a class="pagination-previous">Previous</a>
+              <a class="pagination-next">Next page</a>
+              <ul class="pagination-list">
+                <li>
+                  <a class="pagination-link" aria-label="Goto page 1">1</a>
+                </li>
+                <li><span class="pagination-ellipsis">&hellip;</span></li>
+                <li>
+                  <a class="pagination-link" aria-label="Goto page 45">45</a>
+                </li>
+                <li>
+                  <a
+                    class="pagination-link is-current"
+                    aria-label="Page 46"
+                    aria-current="page"
+                    >46</a
+                  >
+                </li>
+                <li>
+                  <a class="pagination-link" aria-label="Goto page 47">47</a>
+                </li>
+                <li><span class="pagination-ellipsis">&hellip;</span></li>
+                <li>
+                  <a class="pagination-link" aria-label="Goto page 86">86</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </tr>
       </tbody>
     </table>
   </div>
@@ -84,6 +120,7 @@ export default {
     actionShow: Boolean,
     keyEvent: String,
     page: Number,
+    totalPage: Number,
   },
   data: function () {
     return {};
@@ -125,5 +162,10 @@ export default {
 <style scoped>
 .padded {
   padding: 25px;
+}
+.pagination-page {
+  padding: 2rem;
+  width: 100% !important;
+  position: absolute;
 }
 </style>
